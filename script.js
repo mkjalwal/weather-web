534836ce39fb31ccae2a7cec68f19d35f3471888
Manjeet kumar <manjeetjalwal11@gmail.com>
2025-12-10 11:17:21 +0530

    add live location feature

--- script.js
+++ script.js
@@ -5,18 +5,40 @@ const citytime= document.getElementById("city-time");
 const citytemp= document.getElementById("city-temp");
 
 
-async function getData(cityName){
+async function getData(lat,long){
   const promise= await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=670adb72e28e46589e861405250712&q=${cityName}&aqi=yes`
+    `http://api.weatherapi.com/v1/current.json?key=670adb72e28e46589e861405250712&q=${lat},${long}&aqi=yes`
 );
   return await promise.json();
 }
 
-button.addEventListener("click", async () => {
-    const value = input.value;
-    const result = await getData(value);
-    cityname.innerText = `${result.location.name},${result.location.region},${result.location.country}`;
-    citytime.innerText = `${result.location.localtime}`;
-    citytemp.innerText = `${result.current.temp_c}`;
+async function gotlocation(position){
+  const result= await getData(
+    position.coords.latitude,
+    position.coords.longitude
+  )
+  cityname.innerText = `${result.location.name},${result.location.region},${result.location.country}`;
+  citytime.innerText = `${result.location.localtime}`;
+  citytemp.innerText = `${result.current.temp_c}`;
+}
+
+function failedtoget(){
+  console.log("There is some error");
+}
+
+button.addEventListener("click" , async () =>{
+ const result = navigator.geolocation.getCurrentPosition(gotlocation,failedtoget);
+   // cityname.innerText = `${result.location.name},${result.location.region},${result.location.country}`;
+   // citytime.innerText = `${result.location.localtime}`;
+    //citytemp.innerText = `${result.current.temp_c}`;
 });
+
+//button.addEventListener("click", async () => {
+   // const value = input.value;
+    //const result = await getData(value);
+    //const result = await gotlocation(position);
+    //cityname.innerText = `${result.location.name},${result.location.region},${result.location.country}`;
+   // citytime.innerText = `${result.location.localtime}`;
+   // citytemp.innerText = `${result.current.temp_c}`;
+//});
 
